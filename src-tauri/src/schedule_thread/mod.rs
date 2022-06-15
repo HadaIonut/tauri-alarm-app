ea135c4974b337b8d7bf2fad9e55d9c305c62550
ionut <hadaionut@gmail.com>
2022-06-15 22:27:26 +0300

    added alarm notification and alarm noise :)

--- src-tauri/src/schedule_thread/mod.rs
+++ src-tauri/src/schedule_thread/mod.rs
@@ -5,6 +5,7 @@ use std::{thread, time};
 use chrono::Local;
 use tauri::{State, Window, Wry};
 use crate::ScheduleMessage;
+use std::process::Command;
 
 pub mod schedule_message;
 pub mod alarms_changed_payload;
@@ -12,6 +13,30 @@ pub mod alarms_changed_payload;
 #[derive(Default)]
 pub struct RunningAlarms {
     pub time_stamp_map: Arc<Mutex<HashMap<String, ScheduleMessage>>>,
+}
+
+fn trigger_notification(notification_message: String) {
+    let command = "zenity --info --title \"Tauri Alarm\" --text \"".to_string()
+        + &*notification_message + &*"\" --timeout=2".to_string();
+    let mut notification = Command::new("sh");
+    notification.arg("-c").arg(command).output().expect("failed to execute process");
+}
+
+fn play_sound() {
+    use soloud::*;
+    let sl = Soloud::default().unwrap();
+    let mut wav = audio::Wav::default();
+    wav.load_mem(include_bytes!("../../alarm/alarm1.wav")).unwrap();
+    sl.play(&wav);
+    while sl.voice_count() > 0 {
+        sleep(time::Duration::from_millis(100));
+    }
+}
+
+fn trigger_alarm(notification_message: String) {
+    thread::spawn(move || { trigger_notification(notification_message)});
+    thread::spawn(move || { play_sound()});
+
 }
 
 #[tauri::command(async)]
@@ -24,7 +49,7 @@ pub fn start_schedule_thread(to_start_alarms_state: State<RunningAlarms>, window
             if data.len() > 0 {
                 data.retain(|_key, val| -> bool {
                     if val.execute_time_stamp.timestamp() <= Local::now().timestamp() {
-                        println!("Executed: {}", val.message);
+                        trigger_alarm(val.message.clone());
                         let payload = val.to_alarm_payload();
                         window.emit("alarm-removed",  payload).unwrap();
                     }
